b8750ef8fa21661cc7fa0f1e4dbacd8172700529
passionate-laravel-dev <taha.engineer26@gmail.com>
2025-06-03 04:21:24 +0200

    code style fix

--- src/API/GeneralQuery.php
+++ src/API/GeneralQuery.php
@@ -12,52 +12,62 @@ class GeneralQuery
     public function __construct(protected BaseApiClient $client) {}
 
     /**
-     * caption list 
-     * 
+     * caption list
+     *
      * @see https://apifox.com/apidoc/shared/115a0d85-28a5-479d-8b8b-83d7898a7246/api-289343428
      */
-    public function captionList(): JsonResponse {
+    public function captionList(): JsonResponse
+    {
         $res = $this->client->request('get', 'v1/caption/list');
+
         return $this->client->jsonStatusResponse($res);
     }
 
     /**
      * voice query
-     * 
+     *
      * @see https://apifox.com/apidoc/shared/115a0d85-28a5-479d-8b8b-83d7898a7246/api-273646924
      */
-    public function voiceQuery(?array $params = []): JsonResponse {
+    public function voiceQuery(?array $params = []): JsonResponse
+    {
         $res = $this->client->request('get', 'v1/voice/query', $params);
+
         return $this->client->jsonStatusResponse($res);
     }
 
     /**
      * ai avatar query
-     * 
+     *
      * @see https://apifox.com/apidoc/shared/115a0d85-28a5-479d-8b8b-83d7898a7246/api-273646925
      */
-    public function aiAvatarQuery(?array $params = []) {
+    public function aiAvatarQuery(?array $params = [])
+    {
         $res = $this->client->request('get', 'v1/aiavatar/query', $params);
+
         return $this->client->jsonStatusResponse($res);
     }
 
     /**
      * check notice url
-     * 
+     *
      * @see https://apifox.com/apidoc/shared/115a0d85-28a5-479d-8b8b-83d7898a7246/api-298323513;
      */
-    public function checkNoticeUrl(string $noticeUrl): JsonResponse {
+    public function checkNoticeUrl(string $noticeUrl): JsonResponse
+    {
         $res = $this->client->request('post', 'notice/notice_url/check', compact('noticeUrl'));
+
         return $this->client->jsonStatusResponse($res);
     }
 
     /**
      * ethnicity query
-     * 
+     *
      * @see https://apifox.com/apidoc/shared/115a0d85-28a5-479d-8b8b-83d7898a7246/api-296774803
      */
-    public function ethnicityQuery(array $params): JsonResponse {
+    public function ethnicityQuery(array $params): JsonResponse
+    {
         $res = $this->client->request('get', 'v1/ethnicity/list', $params);
+
         return $this->client->jsonStatusResponse($res);
     }
 }
--- src/API/VideoAvatar.php
+++ src/API/VideoAvatar.php
@@ -10,22 +10,25 @@ class VideoAvatar
 
     /**
      * submit task
-     * 
+     *
      * @see https://apifox.com/apidoc/shared/115a0d85-28a5-479d-8b8b-83d7898a7246/api-293375166
      */
     public function submitTask(array $params): JsonResponse
     {
         $res = $this->client->request('post', 'v1/video_avatar/task/submit', $params);
+
         return $this->client->jsonStatusResponse($res);
     }
 
     /**
      * query task
-     * 
+     *
      * @see https://apifox.com/apidoc/shared/115a0d85-28a5-479d-8b8b-83d7898a7246/api-293377750
      */
-    public function queryTask(string $taskId, bool $needCloudFrontUrl = true): JsonResponse {
+    public function queryTask(string $taskId, bool $needCloudFrontUrl = true): JsonResponse
+    {
         $res = $this->client->request('get', '1/video_avatar/task/query', compact('taskId', 'needCloudFrontUrl'));
+
         return $this->client->jsonStatusResponse($res);
     }
 }
